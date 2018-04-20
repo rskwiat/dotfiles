b464f9574bbdd5b138f984c00f72962fa9698ca1
Robert Skwiat <robert.skwiat@mastercard.com>
2018-04-20 09:39:31 -0400

    updating sublime settings and adding in user settings for vscode

---
+++ vscode/usersettings.js
@@ -0,0 +1,13 @@
+{
+  "editor.fontSize": 15,
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "editor.fontFamily": "Fira Code",
+  "editor.fontLigatures": true,
+  "http.proxy": "https://10.203.0.1:5187/",
+  "http.proxyStrictSSL": false,
+  "workbench.colorTheme": "Afterglow Remastered",
+  "workbench.startupEditor": "newUntitledFile",
+  "window.zoomLevel": 0,
+  "terminal.integrated.fontSize": 15,
+}
